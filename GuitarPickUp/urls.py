1f23a289a92bfd765862eed98992a6ba35a3c289
unknown <karim1809252@miuegypt.edu.eg>
2021-12-25 10:55:23 -0800

    changes

--- GuitarPickUp/urls.py
+++ GuitarPickUp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CustomLoginView,RegisterPage
+from .views import CustomLoginView,RegisterPage,mediapipePage
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', RegisterPage.as_view(), name='register'),
+    path('show/', mediapipePage, name='show'),
 ]
--- GuitarPickUp/views.py
+++ GuitarPickUp/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from typing import List
+from .models import *
+from django.core.mail import EmailMessage
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+import cv2
+import threading
+import mediapipe as mp
+import time
 from django.db import models
 from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
@@ -13,7 +21,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def home(request):
     return render(request, 'base/home.html')
@@ -38,3 +45,86 @@ class RegisterPage(FormView):
         if user is not None:
             login(self.request,user)
         return super(RegisterPage,self).form_valid(form)
+@gzip.gzip_page
+def mediapipePage(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
+    return render(request, 'app1.html')
+
+#to capture video class
+class VideoCamera(object):
+    def __init__(self):
+        self.detector = handDetector()
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frames) = self.video.read()
+        self.frame = self.detector.find_hands(self.frames)
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frames) = self.video.read()
+            self.frame = self.detector.find_hands(self.frames)
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+class handDetector():
+    def __init__(self,mode = False,max_hands = 2,detection_confidence = 0.5,trackConfidence = 0.5):
+        self.mode = mode
+        self.max_hands = max_hands
+        self.detection_confidence = detection_confidence
+        self.trackConfidence = trackConfidence
+        self.mpHands = mp.solutions.hands
+        
+        self.hands = self.mpHands.Hands(self.mode,self.max_hands,1,self.detection_confidence,self.trackConfidence)
+        self.mpDraw = mp.solutions.drawing_utils
+        
+    def find_hands(self,img,draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+        if(self.results.multi_hand_landmarks):
+            for handLms in self.results.multi_hand_landmarks:   
+                if(draw):
+                    print(self.results.multi_handedness)
+                    self.mpDraw.draw_landmarks(img, handLms,self.mpHands.HAND_CONNECTIONS)
+        
+        return img
+    
+    def find_position(self,img,handNo = 0,draw = False):
+        lmList = []
+        if(self.results.multi_hand_landmarks):
+            myHand = self.results.multi_hand_landmarks[handNo]
+            
+            for id,lm in enumerate(myHand.landmark):
+                #print(id,lm)
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w) , int(lm.y*h)
+                #print(id,cx,cy)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),25,(255,0,255),cv2.FILLED)
+                
+        return lmList
+            
+            #id is the landmark
+            #4,8,12,16,20
+            #if(id == 4):
+                #cv2.circle(img,(cx,cy),25,(255,0,255),cv2.FILLED)
+            #    print(id,str(cx*lm.z),str(cy*lm.z))
+    
+    
