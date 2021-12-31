58a1f32b54d1ae850baa32bf870d6eb0c17065aa
Ahmed Yehia <ahmedyehia2576@gmail.com>
2021-12-31 15:31:15 +0200

    class diagram and settings updated

--- GuitarPickUp_Project/settings.py
+++ GuitarPickUp_Project/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
