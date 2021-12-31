9c0eda7cf053cbb5afbb384c88f7a9d05e7c39df
Ahmed Yehia <ahmedyehia2576@gmail.com>
2021-12-31 15:16:35 +0200

    models added

--- GuitarPickUp/models.py
+++ GuitarPickUp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Excercise(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    title = models.CharField(max_length=200) 
+    positions = models.TextField(null=True,blank=True)
+    path = models.TextField(null=True,blank=True)
+    played = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering =['played']
+
+class StudentVideo(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    Excercise_id = models.ForeignKey(Excercise,on_delete=models.CASCADE,null=True,blank=True)
+    feedback_id = models.IntegerField(null=True,blank=True)
+    Path = models.TextField(null=True,blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering =['user_id']
+
+
+class Feedback(models.Model):
+    video_id = models.ForeignKey(StudentVideo,on_delete=models.CASCADE,null=True,blank=True)
+    feedback = models.TextField(null=True,blank=True)
+    report= models.TextField(null=True,blank=True)
+
+    def __str__(self) -> str:
+        return self.feedback
+
+    class Meta:
+        ordering =['video_id']
--- GuitarPickUp_Project/settings.py
+++ GuitarPickUp_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GuitarPickUp.apps.GuitarpickupConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
